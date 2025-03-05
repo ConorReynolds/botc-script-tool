469ef9bcd67844976551ba42d7b902900fa3bc62
Nicholas Blott <blottn@tcd.ie>
2025-03-05 19:00:24 +0100

    Move encoding to separate file
    Allows for a more modular importing

---
+++ src/encoding.js
@@ -0,0 +1,63 @@
+import { Character } from "./character.js";
+import { Script } from "./script.js";
+
+export function decompressScript(str) {
+  const array = str.split("~");
+
+  const chars = Character.flat.map((o) => o.id);
+  const fabled = Character.fabledFlat.map((o) => o.id);
+  const homebrew = Character.homebrewFlat.map((o) => o.id);
+
+  const localScript = new Script();
+  localScript.isRecording = false;
+  localScript.name = array[0];
+  localScript.author = array[1];
+
+  for (let i = 2; i < array.length; i++) {
+    const key = array[i];
+    const idx = parseInt(key);
+    if (key.includes("f")) {
+      const char = new Character(fabled[idx]);
+      localScript.add(char);
+    } else if (key.includes("h")) {
+      const char = new Character(homebrew[idx]);
+      localScript.add(char);
+    } else {
+      const char = new Character(chars[idx]);
+      localScript.add(char);
+    }
+  }
+
+  localScript.timeline.addInstant(localScript.toJSON());
+  localScript.timeline.forget();
+  return localScript;
+}
+
+export function compressScript(appState) {
+  // Only works for non-custom scripts
+  const name = appState.currentScript.name;
+  const author = appState.currentScript.author;
+  const chars = Array.from(appState.currentScript.charSet);
+
+  chars.sort();
+
+  const charsMap = {};
+  for (const [i, c] of Character.flat.map((o) => o.id).entries()) {
+    charsMap[c] = `${i}`;
+  }
+  for (const [i, c] of Character.fabledFlat.map((o) => o.id).entries()) {
+    charsMap[c] = `${i}f`;
+  }
+  for (const [i, c] of Character.homebrewFlat.map((o) => o.id).entries()) {
+    charsMap[c] = `${i}h`;
+  }
+
+  const indices = chars.map((c) => charsMap[c]);
+  let str = JSON.stringify(indices);
+  str = str.replaceAll('"', "");
+  str = str.replaceAll(",", "~");
+  str = str.replaceAll(/\[|\]/g, "");
+  str = `${name}~${author}~${str}`;
+  return str;
+}
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { AppState } from "./state.js";
 import { Character } from "./character.js";
 import { Script } from "./script.js";
+import { decompressScript, compressScript } from "./encoding.js";
 import { compareOn, moveElem } from "./utils.js";
 
 let h1;
@@ -126,66 +127,6 @@ function toggleLock(width) {
   metadata.style.width = width;
 }
 
-function compressScript() {
-  // Only works for non-custom scripts
-  const name = appState.currentScript.name;
-  const author = appState.currentScript.author;
-  const chars = Array.from(appState.currentScript.charSet);
-
-  chars.sort();
-
-  const charsMap = {};
-  for (const [i, c] of Character.flat.map((o) => o.id).entries()) {
-    charsMap[c] = `${i}`;
-  }
-  for (const [i, c] of Character.fabledFlat.map((o) => o.id).entries()) {
-    charsMap[c] = `${i}f`;
-  }
-  for (const [i, c] of Character.homebrewFlat.map((o) => o.id).entries()) {
-    charsMap[c] = `${i}h`;
-  }
-
-  const indices = chars.map((c) => charsMap[c]);
-  let str = JSON.stringify(indices);
-  str = str.replaceAll('"', "");
-  str = str.replaceAll(",", "~");
-  str = str.replaceAll(/\[|\]/g, "");
-  str = `${name}~${author}~${str}`;
-  return str;
-}
-
-function decompressScript(str) {
-  const array = str.split("~");
-
-  const chars = Character.flat.map((o) => o.id);
-  const fabled = Character.fabledFlat.map((o) => o.id);
-  const homebrew = Character.homebrewFlat.map((o) => o.id);
-
-  const localScript = new Script();
-  localScript.isRecording = false;
-  localScript.name = array[0];
-  localScript.author = array[1];
-
-  for (let i = 2; i < array.length; i++) {
-    const key = array[i];
-    const idx = parseInt(key);
-    if (key.includes("f")) {
-      const char = new Character(fabled[idx]);
-      localScript.add(char);
-    } else if (key.includes("h")) {
-      const char = new Character(homebrew[idx]);
-      localScript.add(char);
-    } else {
-      const char = new Character(chars[idx]);
-      localScript.add(char);
-    }
-  }
-
-  localScript.timeline.addInstant(localScript.toJSON());
-  localScript.timeline.forget();
-  return localScript;
-}
-
 function firstNightUpdate(script, event) {
   if (script.firstNightOrder === undefined) {
     return;
@@ -284,7 +225,7 @@ function renderScript() {
 function updateScriptLink() {
   const url = new URL(globalThis.location.href);
   if (url.searchParams.has("script")) {
-    const encodedScript = compressScript();
+    const encodedScript = compressScript(appState);
     globalThis.history.replaceState(
       null,
       "",
@@ -727,7 +668,7 @@ globalThis.addEventListener("DOMContentLoaded", () => {
       if (url.searchParams.has("script")) {
         globalThis.history.replaceState(null, "", globalThis.location.pathname);
       } else {
-        const encodedScript = compressScript();
+        const encodedScript = compressScript(appState);
         globalThis.history.replaceState(
           null,
           "",
